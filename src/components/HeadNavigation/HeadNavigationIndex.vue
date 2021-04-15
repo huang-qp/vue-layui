55864714816dbc14ca796705e0b87285e13e5b97
huangqiping <898666716@qq.com>
2021-04-16 00:58:31 +0800

    左侧导航滚动条，面包屑添加

--- src/components/HeadNavigation/HeadNavigationIndex.vue
+++ src/components/HeadNavigation/HeadNavigationIndex.vue
@@ -9,8 +9,12 @@
                     background-color="#545c64"
                     text-color="#fff"
                     active-text-color="#ffd04b">
-                <el-menu-item index="1">处理中心</el-menu-item>
-                <el-submenu index="2">
+                <el-menu-item class="headNavigationRight" index="0" disabled><h2>后台管理系统</h2></el-menu-item>
+                <div class="headNavigationRightSearch" index="9">
+                    <el-input placeholder="请输入查询内容" v-model="searchValue"></el-input>
+                </div>
+                <el-menu-item class="headNavigationLeft" index="1">处理中心</el-menu-item>
+                <el-submenu class="headNavigationLeft" index="2">
                     <template slot="title">我的工作台</template>
                     <el-menu-item index="2-1">选项1</el-menu-item>
                     <el-menu-item index="2-2">选项2</el-menu-item>
@@ -22,8 +26,8 @@
                         <el-menu-item index="2-4-3">选项3</el-menu-item>
                     </el-submenu>
                 </el-submenu>
-                <el-menu-item index="3" disabled>消息中心</el-menu-item>
-                <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+                <el-menu-item class="headNavigationLeft" index="3" disabled>消息中心</el-menu-item>
+                <el-menu-item class="headNavigationLeft" index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
             </el-menu>
         </div>
     </div>
@@ -41,7 +45,8 @@
 		data() {
 			return {
 				activeIndex: '1',
-				activeIndex2: '1'
+				activeIndex2: '1',
+                searchValue:''
 			};
 		},
 		
@@ -54,6 +59,27 @@
 	}
 </script>
 
-<style scoped>
-
+<style lang='scss' scoped>
+    #HeadNavigationIndex{
+        .HeadNavigationContent{
+            .el-menu-demo.el-menu--horizontal.el-menu{
+                .headNavigationLeft{
+                    float: right;
+                }
+                .headNavigationRight{
+                    line-height: 30px;
+                    width: 200px;
+                    cursor: default;
+                    h2{
+                        text-align: center;
+                    }
+                }
+                .headNavigationRightSearch{
+                        float: left;
+                        height: 60px;
+                        line-height: 60px;
+                }
+            }
+        }
+    }
 </style>
--- src/views/homePage/homePageIndex.vue
+++ src/views/homePage/homePageIndex.vue
@@ -6,11 +6,22 @@
                     <HeadNavigationIndex/>
                 </el-header>
                 <el-container>
-                    <el-aside style="width:auto;">
-                        <SideVavigationIndex/>
-                    </el-aside>
+                    <el-scrollbar style="height:100%;">
+                        <el-aside style="width:auto;">
+                            <SideVavigationIndex/>
+                        </el-aside>
+                    </el-scrollbar>
                     <el-container>
-                        <el-main>Main</el-main>
+                        <el-main>
+                           <div>
+                               <el-breadcrumb separator-class="el-icon-arrow-right">
+                                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                                <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+                                <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                                <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+                                </el-breadcrumb>
+                           </div>
+                        </el-main>
                         <el-footer>Footer</el-footer>
                     </el-container>
                 </el-container>
@@ -61,18 +72,28 @@
 
                 .el-container {
                     height: 100%;
-
-                    .el-aside {
-                        position: relative;
-                        overflow-y: scroll;
+                    .el-scrollbar{
+                        /deep/ .el-scrollbar__wrap{//不加deep样式不生效 侧边导航会出现横向滚动条
+                            overflow-x: hidden;
+                            background-color: rgb(84, 92, 100);
+                            .el-aside {
+                            position: relative;
+                            // overflow-y: scroll;
+                            }
+                        }
                     }
-
+                    
                     .el-main {
-
+                        overflow-y: scroll;
+                        z-index: 10;
                     }
 
                     .el-footer {
                         background-color: #ccc;
+                        height: 60px;
+                        position: fixed;
+                        bottom: 0;
+                        width: 100%;
                     }
                 }
             }
